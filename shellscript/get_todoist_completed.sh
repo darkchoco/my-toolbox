e482faa57b1e7a2d45603884e178d10c5f0dbe61
darkchoco <darkchoco@cloudscape.kr>
2023-02-01 13:07:28 +0100

    get_todoist_completed.sh added.

---
+++ shellscript/get_todoist_completed.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+
+# DESCRIPTION:
+#   Getting the list of completed tasks from Todoist.
+#   crontab에 아래와 같이 등록해서 사용한다.
+#
+#   SHELL=/bin/bash
+#   05 23 * * 0 /home/ubuntu/bin/get_todoist_completed.sh 2>&1
+#
+# OPTIONS:
+#
+# EXAMPLE:
+#   get_todoist_completed.sh
+#
+# REFERENCE:
+#   https://www.baeldung.com/linux/bash-variables-export
+
+source ./env.sh
+
+curl https://api.todoist.com/sync/v9/completed/get_all \
+  -H "Authorization: Bearer $todoist" \
+  -d command='[
+  {
+    "since"=$(date -d "yesterday" '+%Y-%m-%d')T23:00:00.000000Z,
+  }]' \
+  | jq '.' \
+  > /home/ubuntu/dat/completed_$(date '+%Y%m%d').json
