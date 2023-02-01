8895d3deab6d3636c9783e6c16d7e3f539e06758
darkchoco <darkchoco@cloudscape.kr>
2023-02-01 13:06:47 +0100

    .gitignore, template.sh added.

---
+++ shellscript/template.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+# DESCRIPTION:
+#   This is a script template.
+#
+# OPTIONS:
+#   -o [file], --output=[file]    Set log file (default=/dev/null)
+#                                 use DEFAULT keyword to autoname file
+#   -t, --timelog                 Add timestamp to log ("+%y/%m/%d@%H:%M:%S")
+#   -h, --help                    Print this help
+#   -v, --version                 Print script information
+#
+# EXAMPLE:
+#   ${SCRIPT_NAME} -o DEFAULT arg1 arg2
+#
+# REFERENCE:
+#   https://github.com/darkchoco
